42390315c2a391380e75c4c4da66ffd339aeec6a
luhaoyu <luhaoyu@kuaishou.com>
2022-03-13 22:13:50 +0800

    tmp commit

--- 两数之和.ts
+++ 两数之和.ts
@@ -1,3 +1,10 @@
-// function twoSum(nums: number[], target: number): number[] {
-//
-// }
+function twoSum(nums: number[], target: number): number[] {
+    let map = new Map<number, number>();
+    for(let key in nums) {
+        let l = target - nums[key];
+        if(map[l] !== undefined){
+            return [map[l],key];
+        }
+        map[nums[key]] = key;
+    }
+}
